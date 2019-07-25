a186fdf8b4e5342072af8fc7e8f7752929d646ca
jumpman <jbollman@protonmail.com>
2019-07-25 15:45:30 -0600

    breaking out slave and master install scripts

---
+++ Kubernetes-install-slave-centos.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#Turn of fswap
+swapoff -a
+
+# vim /etc/fstab and comment out swap line
+
+yum -y update
+
+yum -y install docker
+
+systemctl enable docker
+systemctl start docker
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+setenforce 0
+
+yum install -y kubelet kubeadm kubectl
+
+systemctl enable kubelet
+
+systemctl start kubelet
+
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sysctl --system
+
+kubeadm init --pod-network-cidr=10.170.0.0/16 #assumes you want it on this network
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config 
